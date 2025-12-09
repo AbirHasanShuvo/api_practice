cebd774ec7e6e3ae3715584ca274a61d569f84a1
Md. Abir Hasan <abirhasanshuvo789@gmail.com>
2025-12-09 11:04:55 +0600

    without routing like is also completed

--- app/Http/Controllers/API/LikeController.php
+++ app/Http/Controllers/API/LikeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Like;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class LikeController extends Controller
 {
@@ -15,13 +17,73 @@ class LikeController extends Controller
         //
     }
 
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'post_id' => 'required|integer|exists:blog_posts,id',
+            'status' => 'required|integer'
+
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'Failed',
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        $userId = auth()->id();;
+        $postId = $request->post_id;
+        $status = $request->status; // 1 for like, 0 for dislike
+
+        //if user has already reacted to the post, update the reaction
+        $like = Like::where('user_id', $userId)->where('post_id', $postId)->first();
+
+        if ($like) {
+            if ($like->status == $status) {
+                //same reaction, remove it
+                $like->delete();
+
+                return response()->json([
+                    'status' => 'Success',
+                    'message' => 'Reaction removed successfully'
+                ], 201);
+            } else {
+                $like->status = $status;
+                $like->save();
+                return response()->json([
+                    'status' => 'Success',
+                    'message' => 'Reaction updated successfully'
+                ], 201);
+            }
+        } else {
+            Like::create(
+                [
+                    'user_id' => $userId,
+                    'post_id' => $postId,
+                    'status' => $status
+                ]
+            );
+
+            return response()->json([
+                'status' => 'Success',
+                'message' => 'Reaction added'
+            ], 201);
+        }
+
+
+
+
+
+
+        // TODO: Store the like/dislike record for the user and post
+    }
+
+
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
-    }
+
 
     /**
      * Display the specified resource.
